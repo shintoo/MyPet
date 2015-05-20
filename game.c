5ee2d4f1afed93a4f041c84aa77bb892cf0af2f1
shintoo <seanprapp@gmail.com>
2015-05-20 00:48:56 -0400

    Sell to shops

--- game.c
+++ game.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 #include "pet.h"
 #include "owner.h"
@@ -16,7 +17,7 @@ void Game_Init(Owner **Player, Pet **MyPet) {
 	char PetName[MAXST];
 
 	*Player = Owner_New();
-	printf("Welcome to MyPets!\nWhat is your name?\nName: ");
+	printf("\n\n\n\n\n\n\n\n\n\nWelcome to MyPets!\nWhat is your name?\nName: ");
 	s_gets(PlayerName, MAXST);
 	(*Player)->SetName(*Player, PlayerName);
 	
@@ -28,7 +29,8 @@ void Game_Init(Owner **Player, Pet **MyPet) {
 	}
 */	(*MyPet) = Pet_New('d');
 
-	printf("What would you like to name your %s? ", (*MyPet)->type == 'd' ? "dog" : "cat");
+	printf("What would you like to name your %s? ",
+	        (*MyPet)->type == 'd' ? "dog" : "cat");
 	s_gets(PetName, MAXST);
 	(*MyPet)->SetName(*MyPet, PetName);
 
@@ -72,7 +74,8 @@ void Game_Loop(Owner *Player, Pet *MyPet) {
 }
 
 void Game_Exit(Owner *Player, Pet *MyPet) {
-	printf("%s %s goodbye!\n", MyPet->name, MyPet->type == 'd' ? "barks" : "meows");
+	printf("%s %s goodbye!\n",
+	        MyPet->name, MyPet->type == 'd' ? "barks" : "meows");
 	sleep(1);
 	free(Player->name);
 	free(Player->Inventory);
@@ -109,15 +112,16 @@ char ShowMenu(Owner *Player, Pet *MyPet) {
 		case 8 ... 9: coat = "Clean"; break;
 		case 10:      coat = "Perfect"; break;
 	}
-	printf("\n\n\n\n\n\n\n%s\tCash: $%d\n", Player->name, Player->money);
-	printf("%s\t\t%s\n", MyPet->name, MyPet->type == 'd' ? "Dog" : "Cat");
-	printf("\tAge:\t%d\nStatus:\n\tHunger:\t%s\n\tThirst:\t%s\n\tCoat:\t%s\n\n",
+
+	printf("\n\n\n\n\n\n\n%s\t\t\t\t\tCash: $%d\n", Player->name, Player->money);
+	printf("%s\t%s\n", MyPet->name, MyPet->type == 'd' ? "Dog" : "Cat");
+	printf(" Age:\t%d\n Status:\n\tHunger:\t%s\n\tThirst:\t%s\n\tCoat:\t%s\n\n",
 	       MyPet->age, hunger, thirst, coat);
 
 	printf("p) Play with %s\tf) Feed %s\n"
-		   "b) Bathe %s\t\ts) Go to the Shop\nw) Walk\t\t\tq) Quit\n",
-		   MyPet->name, MyPet->name, MyPet->name);
-	printf("What would you like to do? ");
+		   "b) Bathe %s\t\ts) Go to the Shop\nw) Walk %s\t\tq) Quit\n",
+		   MyPet->name, MyPet->name, MyPet->name, MyPet->name);
+	printf("\nWhat would you like to do? ");
 	choice = chget();
 	while (strchr("PpFfBbSsWwQq", choice) == NULL) {
 		printf("Please enter an option from the menu (p, f, w, s, or q): ");
@@ -128,28 +132,91 @@ char ShowMenu(Owner *Player, Pet *MyPet) {
 }
 
 void Shop(Owner *Player) {
+	char buyorsell;
 	int choice;
+	int itemsowned = 0;
+	bool emptyinventory = true;
+	int sellcount;
 
-	printf("\n\n\n\n\n\nWelcome to MyPets Shop!\nCatalog:\n");
-	for (int i = 0; i < 12; i++) {
-		printf("%2d) $%2d %s (%d)\t", i, ShopCatalog[i].price, ShopCatalog[i].name, ShopCatalog[i].count);
-		if ((i+1) % 2 == 0 && i > 0) {
+	printf("\n\n\n\n\n\n\n\nWelcome to MyPet Shop!\n"
+	       "How can I help you?\n\n"
+	       "b) Buy supplies\t\ts) Sell items\n\n"
+	       "What would you like to do? ");
+	buyorsell = chget();
+	while (buyorsell != 'b' && buyorsell != 's') {
+		printf("Please enter 'b' or 's' to buy or sell: ");
+		buyorsell = chget();
+	}
+	puts("\n\n\n\nMyPet Shop\n\n");
+	switch(buyorsell) {
+		case 'b':
+			puts("Catalog:\n");
+			for (int i = 0; i < 12; i++) {
+				printf("%2d) $%2d %s (%d)\t",
+				       i, ShopCatalog[i].price, ShopCatalog[i].name,
+				       ShopCatalog[i].count);
+				if ((i+1) % 2 == 0 && i > 0) {
+					putchar('\n');
+				}
+			}
 			putchar('\n');
-		}
+			printf("What would you like to buy? ");
+			scanf("%d", &choice);
+			getchar();
+			while (choice < 0 || choice > 11) {
+				printf("Please enter a number between 0 and 10: ");
+				choice = chget();
+				choice -= '0';
+			}
+			Player->Inventory[choice] += ShopCatalog[choice].count;
+			Player->money -= ShopCatalog[choice].price;
+			printf("%d %s added to your inventory.\n"
+			       "Thank you for shopping at the MyPets Shop!\n",
+				   ShopCatalog[choice].count, ShopCatalog[choice].name);
+		break;
+		case 's':
+			for (int i = 0; i < 12; i++) {
+				if (Player->Inventory[i] > 0) {
+					emptyinventory = false;
+					itemsowned++;
+				}
+			}
+			if (emptyinventory == true) {
+				puts("Your inventory is empty!");
+				break;
+			}
+			puts("Your inventory:");
+			for (int i = 0; i < 12; i++) {
+				if (Player->Inventory[i] != 0) {
+					printf("%d ($%2d) %s (%d)\t",
+					       i, ShopCatalog[i].resellValue, 
+					       ShopCatalog[i].name, Player->Inventory[i]);
+				}
+				if (i % itemsowned == 0 && itemsowned > 0) {
+					putchar('\n');
+				}
+			}
+			printf("\nWhat would you like to sell? ");
+			scanf("%d", &choice);
+			if (Player->Inventory[choice] == 0) {
+				printf("Please enter a number from the menu." );
+				scanf("%d", &choice);
+			}
+			printf("How many %s would you like to sell? ", ShopCatalog[choice].name);
+			scanf("%d", &sellcount);
+			while (sellcount > Player->Inventory[choice]) {
+				printf("You only have %d!\nHow many would you like to sell? ",
+				       Player->Inventory[choice]);
+				scanf("%d", &sellcount);
+			}
+			Player->Inventory[choice] -= sellcount;
+			Player->money += ShopCatalog[choice].resellValue * sellcount;
+			printf("Sold %d %s to the shop.\n$%d added to wallet.\n"
+			       "Thank you for your business!", sellcount, 
+			      ShopCatalog[choice].name, ShopCatalog[choice].resellValue * sellcount);
+		break;
 	}
-	putchar('\n');
-	printf("What would you like to buy? ");
-	scanf("%d", &choice);
-	getchar();
-	while (choice < 0 || choice > 11) {
-		printf("Please enter a number between 0 and 10: ");
-		choice = chget();
-		choice -= '0';
-	}
-	Player->Inventory[choice] += ShopCatalog[choice].count;
-	Player->money -= ShopCatalog[choice].price;
-	printf("Thank you for shopping at the MyPets Shop!\n");
-	sleep(1);
+	sleep(2);
 }
 
 
